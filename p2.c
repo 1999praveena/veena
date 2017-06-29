e118a5b8e9c845b735c9c33859eb786d963dc297
1999praveena <praveenaarokiaraj@gmail.com>
2017-06-29 14:56:27 +0530

    Create p2.c

---
+++ p2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+clrscr();
+printf("\n  enter num");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("even");
+}
+else
+{
+printf("odd");
+}
+getch();
+retuern 0;
+}
